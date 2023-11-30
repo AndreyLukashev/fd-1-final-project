7083a771471be4cc133ae9d3ba818b5396c7b4b2
AndreyLukashev <enrgy2024@internet.ru>
2023-11-30 13:38:29 +0300

    add button

--- main.js
+++ main.js
@@ -6,4 +6,9 @@ import './src/components/typography/typography';
 
 import './src/components/grid/grid.js';
 
-import './src/components/forms/forms';
+import './src/components/forms/forms.js';
+
+import './src/components/button/button.js';
+
+import './src/global/utils.scss';
+
