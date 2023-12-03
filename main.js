bfb512a2a2b43ec4cba172a35106faac5c839fd1
AndreyLukashev <enrgy2024@internet.ru>
2023-12-03 18:11:22 +0300

    add currency

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-import 'reset-css';
+//node_modules packages
+import 'reset-css'
 
-import './main.scss';
+// Global Styles
+import './main.scss'
 
-import './src/components/typography/typography';
+// Import Components
 
+import './src/components/typography/typography.js';
 import './src/components/grid/grid.js';
-
 import './src/components/forms/forms.js';
-
 import './src/components/button/button.js';
-
 import './src/components/app-header/app-header.js';
+import './src/components/main-menu/main-menu.js';
+import './src/components/app-logo/app-logo.js';
+import './src/components/dropdown/dropdown.js';
 
 import './src/global/utils.scss';
-
