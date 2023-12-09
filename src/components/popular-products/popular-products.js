eefa45c1f657dced88b9f991f8fe6795d408779f
AndreyLukashev <enrgy2024@internet.ru>
2023-12-09 14:16:04 +0300

    add swiper

---
+++ src/components/popular-products/popular-products.js
@@ -0,0 +1,36 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+import './popular-products.scss'
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+new Swiper('.popular-products__slider', {
+    modules: [Navigation, Pagination, Autoplay],
+    spaceBetween: 20,
+    loop: true,
+    // slidesPerView: 3,
+    autoplay: true,
+    speed: 1500,
+    // centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        // bulletActiveClass: 'qwekjdshfkjdsh',
+        // bulletClass: 'asdkjhaskdjh'
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // breakpoints: {
+    //     768: {
+    //         slidesPerView: 2,
+    //         autoplay: {
+    //             delay: 2000,
+    //             pauseOnMouseEnter: true,
+    //         },
+    //     }
+    // }
+});
